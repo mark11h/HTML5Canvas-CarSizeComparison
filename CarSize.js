68e89dc5bf00421ef273f7f25b67204d16102da0
Mark Hinchliffe <mark11h@live.co.uk>
2015-09-03 20:36:16 +0100

    Added canvas and functionality to draw rectangle

--- CarSize.js
+++ CarSize.js
@@ -6,5 +6,14 @@ $(function() {
 });
 
 function compareSizes() {
-	console.log('test');
+	var canvas = document.getElementById('canvas');
+	var c = canvas.getContext('2d');
+	
+	c.clearRect(0,0,800,800); //clear the rectangle based on the width and height of the canvas element before redrawing
+	
+	var car = $('#car').val();
+	var width = $('#width').val();
+	var height = $('#height').val();
+	
+	c.fillRect(0,0,width,height); //1 = pixels from left, 2 = pixels from top, 3 = width, 4 = height
 }
